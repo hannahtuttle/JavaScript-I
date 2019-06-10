cc742a4f145c973d7eb203ec1c67be40e5c995f5
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-06-10 15:26:32 -0700

    mvp

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -104,9 +104,9 @@ console.log(oldCars.length);
 let BMWAndAudi = [];
 for (let i = 0; i < inventory.length; i++){
     if (inventory[i]['car_make'] === 'BMW'){
-        BMWAndAudi.push(inventory[i]['car_make'])
+        BMWAndAudi.push(inventory[i])
     }else if (inventory[i]['car_make'] === 'Audi'){
-        BMWAndAudi.push(inventory[i]['car_make'])
+        BMWAndAudi.push(inventory[i])
     }
 }
 console.log(JSON.stringify(BMWAndAudi));
--- assignments/objects.js
+++ assignments/objects.js
@@ -29,7 +29,7 @@ const intern2 = {
   "id": 2,
   "name": "Kennan",
   "email": "kdiben1@tinypic.com",
-  "gender": "M"
+  "gender": "M",
 }
 
 const intern3 = {
@@ -71,8 +71,8 @@ console.log(intern5.gender)
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
-intern2.speak = `Hello my name is ${intern2.name}!`
-console.log(intern2.speak)
+intern2.speak = () => `Hello my name is ${intern2.name}!`
+console.log(intern2.speak())
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
